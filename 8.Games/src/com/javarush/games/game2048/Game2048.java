fd4cf1a64b11e72f355723001f960ca6556bb02a
Art4ine <artemfom01@gmail.com>
2024-10-07 16:16:06 +0300

    Add Solution Games Part 7/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -49,7 +49,7 @@ public class Game2048 extends Game {
     private void drawScene() {
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 4; x++) {
-
+                
                 setCellColoredNumber(x, y, gameField[y][x]);
             }
         }
@@ -67,68 +67,24 @@ public class Game2048 extends Game {
         }
     }
 
-    public void moveUp() {
-        for (int y = 1; y < 4; y++) {
-            for (int x = 0; x < 4; x++) {
-                for (int yx = 0; yx <= y; yx++) {
-                    if (gameField[yx][x] == 0 & gameField[y][x] != 0) {
-                        gameField[yx][x] = gameField[y][x];
-                        gameField[y][x] = 0;
-                    } else if (gameField[yx][x] == gameField[y][x] & gameField[y][x] != 0) {
-                        gameField[yx][x] *= 2;
-                        gameField[y][x] = 0;
-                    }
+    private boolean compressRow(int[] row) {
+        boolean cellMoved = false;
+        int temp = 0;
+
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] != 0) {
+                row[temp] = row[i];
+
+                if (temp != i) {
+                    row[i] = 0;
+                    cellMoved = true;
                 }
+
+                temp += 1;
             }
         }
 
-        drawScene();
-    }
-
-    public void moveLeft() {
-
-    }
-
-    public void moveRight() {
-
-    }
-
-    public void moveDown() {
-        for (int y = 2; y >= 0; y--) {
-            for (int x = 0; x < 4; x++) {
-                for (int yx = 3; yx >= y; yx--) {
-                    if (gameField[yx][x] == 0 & gameField[y][x] != 0) {
-                        gameField[yx][x] = gameField[y][x];
-                        gameField[y][x] = 0;
-                    } else if (gameField[yx][x] == gameField[y][x] & gameField[y][x] != 0) {
-                        gameField[yx][x] *= 2;
-                        gameField[y][x] = 0;
-                    }
-                }
-            }
-
-            for (int x = 0; x < 3; x++) {
-                if (gameField[y][x] == gameField[y][x + 1] & gameField[y][x] != 0) {
-                    gameField[y][x + 1] *= 2;
-                    gameField[y][x] = 0;
-                }
-            }
-        }
-
-        drawScene();
-    }
-
-    @Override
-    public void onKeyPress(Key key) {
-        if (key == Key.DOWN) {
-            moveDown();
-        } else if (key == Key.UP) {
-            moveUp();
-        } else if (key == Key.RIGHT) {
-            moveRight();
-        } else if (key == Key.LEFT) {
-            moveLeft();
-        }
+        return cellMoved;
     }
 
     private void createGame() {

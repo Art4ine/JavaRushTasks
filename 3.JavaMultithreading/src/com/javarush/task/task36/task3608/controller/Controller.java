3d3cfccb6d76d269031f4c9e8950485b1c425d73
Art4ine <artemfom01@gmail.com>
2025-08-21 18:35:46 +0300

    Add Solution MVC 4

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -3,15 +3,24 @@ package com.javarush.task.task36.task3608.controller;
 import com.javarush.task.task36.task3608.model.FakeModel;
 import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.model.ModelData;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
     private Model model;
-
-    public void onShowAllUsers() {
-        model.loadUsers();
-    }
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
     }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
+    public void onShowAllUsers() {
+        model.loadUsers();
+
+        usersView.refresh(model.getModelData());
+    }
+
 }
